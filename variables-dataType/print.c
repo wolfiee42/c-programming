a02d7aece5ae1514428c374613a5cd64a550d822
wolfiee42 <saifalislam2022@gmail.com>
2025-02-13 19:35:24 +0600

    learning about \n and \t in c

---
+++ variables-dataType/print.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world\n");
+    printf("I am a programmer.\n");
+
+    printf("Hola Amigos.\tI am hacker.");
+}
+
+// \n - new line
+// \t - tab
