0215ae6e53a52cde49c90871a355f8815e7109b2
wolfiee42 <saifalislam2022@gmail.com>
2025-02-17 15:04:09 +0600

    learning loop in multiple way.

---
+++ 03.loop/loop_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int sum = 0;
+    int i;
+    int n;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    printf("%lld", sum);
+
+    return 0;
+}
