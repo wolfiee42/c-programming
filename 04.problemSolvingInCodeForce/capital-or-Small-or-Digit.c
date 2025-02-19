a8ecfc8ef32076271b8940f22a10d56e30327c3e
wolfiee42 <saifalislam2022@gmail.com>
2025-02-20 02:18:48 +0600

    solving problem relate to ascii code

---
+++ 04.problemSolvingInCodeForce/capital-or-Small-or-Digit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char X;
+    scanf("%c", &X);
+
+    if (X >= 48 && X <= 57)
+    {
+        printf("IS DIGIT");
+    }
+    else if (X >= 65 && X <= 90)
+    {
+        printf("ALPHA\nIS CAPITAL");
+    }
+    else if (X >= 97 && X <= 122)
+    {
+        printf("ALPHA\nIS SMALL");
+    }
+}
