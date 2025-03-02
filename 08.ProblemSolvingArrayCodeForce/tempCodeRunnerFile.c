a114476eb87b781dbe51071348783cde95b342b2
wolfiee42 <saifalislam2022@gmail.com>
2025-03-03 00:48:52 +0600

    solving prblem num E

---
+++ 08.ProblemSolvingArrayCodeForce/tempCodeRunnerFile.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int length;
+    scanf("%d", &length);
+
+    int arrOfNum[length];
+    for (int i = 0; i < length; i++)
+    {
+        scanf("%d", &arrOfNum[i]);
+    }
+
+    int lowestNum = arrOfNum[0];
+    int position = 1;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (arrOfNum[i] < lowestNum)
+        {
+            lowestNum = arrOfNum[i];
+            position = i + 1;
+        }
+    }
+    printf("lowestNum %d position %d\n", lowestNum, position);
+}
