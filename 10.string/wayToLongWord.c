876cfff94af91bc258e67cd65a7bb12655d23049
wolfiee42 <saifalislam2022@gmail.com>
2025-03-16 12:21:23 +0600

    solving porblem named 'wayToLongWord.c'

---
+++ 10.string/wayToLongWord.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    while (n--)
+    {
+        char word[101];
+        scanf("%s", word);
+
+        int length = strlen(word);
+        if (length > 10)
+        {
+            printf("%c%d%c\n", word[0], length - 2, word[length - 1]);
+        }
+        else
+        {
+            printf("%s\n", word);
+        }
+    }
+}
