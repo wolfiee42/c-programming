d225009efea67d97a8c22abcbd70181b105f04de
wolfiee42 <saifalislam2022@gmail.com>
2025-04-21 18:17:25 +0600

    resolved N. sum of a matrix. CF with recursion

--- 19.recapRecursion/SumofaMatrix.c
+++ 19.recapRecursion/SumofaMatrix.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 
+void sumOfaMatrix(int arr1[100][100], int arr2[100][100], int n, int m, int i, int j)
+{
+    if (i == n)
+        return;
+    if (j == m)
+    {
+        printf("\n");
+        return sumOfaMatrix(arr1, arr2, n, m, i + 1, 0);
+    }
+
+    printf("%d ", arr1[i][j] + arr2[i][j]);
+
+    sumOfaMatrix(arr1, arr2, n, m, i, j + 1);
+}
+
 int main()
 {
     int n, m;
     scanf("%d %d", &n, &m);
 
-    int arr1[n][m];
+    int arr1[100][100];
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -14,7 +29,7 @@ int main()
         }
     }
 
-    int arr2[n][m];
+    int arr2[100][100];
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -33,4 +48,8 @@ int main()
     //     }
     //     printf("\n");
     // }
+
+    // solution02 - recursion
+    sumOfaMatrix(arr1, arr2, n, m, 0, 0);
+    return 0;
 }
