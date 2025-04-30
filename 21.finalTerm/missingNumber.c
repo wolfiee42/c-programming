a25cd8d4f2fb75936c278ec7765ad5832b99a83f
wolfiee42 <saifalislam2022@gmail.com>
2025-04-30 20:11:39 +0600

    solving problem number a in hackerRank

---
+++ 21.finalTerm/missingNumber.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    int d, a, b, c;
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d %d %d %d", &d, &a, &b, &c);
+        int remaining = (d - (a + b + c));
+        printf("%d\n", remaining);
+    }
+}
